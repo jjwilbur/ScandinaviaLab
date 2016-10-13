c1859b1322c10bd83b130b65aa292273f811f985
Spencer Simpson <spencerjsimpson@studentbody.byu.edu>
2016-10-13 13:15:36 -0600

    still working on it

--- js/main.js
+++ js/main.js
@@ -86,18 +86,18 @@ function CarouselDemoCtrl($scope){
   ];
 }
 
-app.directive('carousel', function($timeout) {
-   return {
-      restrict: 'E',
-      scope: {
-        links: '=' 
-      },
-      templateUrl: 'carousel.html',
-      link: function(scope, element) {
-        $timeout(function() {
-          $('.carousel-indicators li',element).first().addClass('active');
-          $('.carousel-inner .item',element).first().addClass('active');
-        });
-      }
-   }
-});
+// app.directive('carousel', function($timeout) {
+//    return {
+//       restrict: 'E',
+//       scope: {
+//         links: '=' 
+//       },
+//       templateUrl: 'carousel.html',
+//       link: function(scope, element) {
+//         $timeout(function() {
+//           $('.carousel-indicators li',element).first().addClass('active');
+//           $('.carousel-inner .item',element).first().addClass('active');
+//         });
+//       }
+//    }
+// });
